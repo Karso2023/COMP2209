dcc077a63f57d19043fc92f4fca2514842b566a2
Karso <karsoo2023@gmail.com>
2024-10-21 00:13:09 +0100

    Update ex2.hs

--- ex2.hs
+++ ex2.hs
@@ -3,15 +3,15 @@ import Data.Char
 
 -- Exercise 2 
 fourth :: [a] -> a
-fourth xs | length xs < 4 = error "Length of list < 4" 
+fourth xs | length xs < 4 = error "Length of list < 4"
           | otherwise = xs !! 3
 
 -- Exercise 3
 safetail :: [a] -> [a]
-safetail xs | null xs = []  
+safetail xs | null xs = []
             | otherwise = tail xs
 
- where null xs = length xs == 0 
+ where null xs = length xs == 0
 
  -- Exercise 4
 halve :: [a] -> ([a],[a])
@@ -26,11 +26,22 @@ halve xs = splitAt (length xs `div` 2) xs
 -- Assessed Exercises A1
 -- group anti-monotone 
 -- anti-monotone if it has no contiguous monotone sublist with 3 distinct values
+-- Approach: if [Int] >= 3 and is sorted, then split it by following anti-monotone rule
+isSortedBoth :: Ord a => [a] -> Bool
+isSortedBoth xs = length xs >= 3 && (isAscending xs || isDescending xs)
+  where
+    isAscending ys = and (zipWith (<=) ys (tail ys))
+    isDescending ys = and (zipWith (>=) ys (tail ys))
+
 amSplit :: [Int] -> [[Int]]
 amSplit [] = []
 amSplit [a] = [[a]]
-amSplit xs | length xs < 3 = [xs]
-           | 
+amSplit xs
+     | isSortedBoth xs = [take n xs, drop n xs]
+     | otherwise = [xs]
+  where n = length xs `div` 2
+
+
 
 
 -- Assessed Exercises A2
