d92f8c4b35b54473db911bc257cbb1fc83b0164c
Karso <karsoo2023@gmail.com>
2024-10-21 01:06:38 +0100

    Update ex2.hs

--- ex2.hs
+++ ex2.hs
@@ -1,4 +1,5 @@
 import Data.Char
+
 -- Exercise 1
 
 -- Exercise 2 
@@ -26,21 +27,27 @@ halve xs = splitAt (length xs `div` 2) xs
 -- Assessed Exercises A1
 -- group anti-monotone 
 -- anti-monotone if it has no contiguous monotone sublist with 3 distinct values
--- Approach: if [Int] >= 3 and is sorted, then split it by following anti-monotone rule
+-- Approach: if [Int] >= 3 and is sorted, then split it by following the anti-monotone rule
 
 isSortedBoth :: Ord a => [a] -> Bool
 isSortedBoth xs = length xs >= 3 && (isAscending xs || isDescending xs)
   where
-    isAscending ys = and (zipWith (<=) ys (tail ys))
-    isDescending ys = and (zipWith (>=) ys (tail ys))
+    isAscending ys = and (zipWith (<) ys (tail ys))
+    isDescending ys = and (zipWith (>) ys (tail ys))
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n list = first : splitEvery n rest
+  where
+    (first,rest) = splitAt n list
 
 amSplit :: [Int] -> [[Int]]
 amSplit [] = []
 amSplit [a] = [[a]]
 amSplit xs
-     | isSortedBoth xs = [take n xs, drop n xs]
-     | otherwise = [xs]
-  where n = length xs `div` 2
+     | isSortedBoth xs = splitEvery 2 xs
+     | otherwise = [xs] 
+  
 
 
 
