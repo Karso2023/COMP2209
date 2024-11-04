789bc98dea1dc6a41ee3f71adbb5a4496aacc1d5
Karso <karsoo2023@gmail.com>
2024-11-04 19:47:33 +0000

    Update ex4.hs

--- ex4.hs
+++ ex4.hs
@@ -3,8 +3,12 @@ sumEvenOdd :: Integer
 sumEvenOdd = sum [ x^2 | x <- [0,2..100] ] + sum [x^3 | x <- [1,3..99]]   
 
 -- Ex2
+grid :: Int -> Int -> [(Int,Int)]
+grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
 
 -- Ex3
+replicate' :: Int -> a -> [a]
+replicate' i s= [s | _ <- [1..i]]
 
 -- Ex4
 
