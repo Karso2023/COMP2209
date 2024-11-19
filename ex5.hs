ab2f43222d257c876b884232ccb279befb1f3ecc
Karso <karsoo2023@gmail.com>
2024-11-19 12:00:00 +0000

    Update ex5.hs

--- ex5.hs
+++ ex5.hs
@@ -20,3 +20,18 @@ absValue :: Int -> Int
 absValue x
      | x > 0 = x 
      | otherwise = -x 
+
+-- -- --
+-- Double linked list 
+data DLList a = Empty | Node a (DLList a) (DLList a)
+mkDLList :: [a] -> DLList a 
+mkDLList [] = Empty 
+mkDLList [x] = Node x Empty Empty
+mkDLList [x1, x2] = let node1 = Node x1 Empty node2 
+                        node2 = Node x2 node1 Empty
+                    in node1
+
+mkDLList [x1, x2, x3] = let node1 = Node x1 Empty node2
+                            node2 = Node x2 node1 node3
+                            node3 = Node x3 node2 Empty
+                        in node1
