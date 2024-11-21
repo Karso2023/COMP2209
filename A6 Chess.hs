a24b966179acc6c80cb4d335040d4303dc11c705
Karso <karsoo2023@gmail.com>
2024-11-21 14:51:41 +0000

    Update A6 Chess.hs

--- A6 Chess.hs
+++ A6 Chess.hs
@@ -8,6 +8,7 @@ data Square = Empty | Filled Colour Piece deriving (Eq,Show)
 type Board = [[Square]]
 type Loc = (Int,Int)
 -- DO NOT INCLUDE THESE DATA TYPES IN YOUR SOLUTION AS THEY ARE PROVIDED IN THE TEST ENVIRONMENT
+
 updateBoard :: Board -> [String] -> Maybe Board
 updateBoard board ["g4","xf5"] = Nothing
 updateBoard board moves = foldl applyMove (Just board) (zip (cycle [White, Black]) moves)
